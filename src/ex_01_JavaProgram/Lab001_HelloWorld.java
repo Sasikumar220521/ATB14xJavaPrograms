6c48c9abc53a03b8b42c6b3bec717e7928c89507
Sasikumar220521 <sasikumar220521@gmail.com>
2025-09-13 22:23:39 +0530

    Commit MMain to test push and pull

--- src/ex_01_JavaProgram/Lab001_HelloWorld.java
+++ src/ex_01_JavaProgram/Lab001_HelloWorld.java
@@ -4,5 +4,6 @@ public class Lab001_HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(3/2.5); // println -> interger part -> no decimal
     }
 }
---
+++ src/ex_02_JavaProgram/Lab003_MMain.java
@@ -0,0 +1,14 @@
+package ex_02_JavaProgram;
+
+public class Lab003_MMain {
+    public static void main(String[] args) {
+//        System.out.println(10+3/2-1*5);
+    String st = "I am sk";
+    String rev_st = "";
+    for(int i = st.length()-1; i>=0; i--){
+        rev_st = rev_st + st.charAt(i);
+        }
+    System.out.println("Original string: "+ st);
+    System.out.println("Final reversed string: "+ rev_st);
+    }
+}
