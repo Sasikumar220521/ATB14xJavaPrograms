fbf6800030c44318f267387aac6ab1c38b2c0352
Sasikumar220521 <sasikumar220521@gmail.com>
2025-11-29 11:49:37 +0530

    Nov 29th Morning

--- src/Logical_Programs/P033_PersonTwoIns.java
+++ src/Logical_Programs/P033_PersonTwoIns.java
@@ -1,16 +1,16 @@
 package Logical_Programs;
 
-public class P032_PersonTwoIns {
+public class P033_PersonTwoIns {
     String name;
     int age;
-    public P032_PersonTwoIns(String name, int age){
+    public P033_PersonTwoIns(String name, int age){
         this.name = name;
         this.age = age;
     }
 
     public static void main(String[] args) {
-        P032_PersonTwoIns john = new P032_PersonTwoIns("John", 25);
-        P032_PersonTwoIns alice = new P032_PersonTwoIns("Alice", 30);
+        P033_PersonTwoIns john = new P033_PersonTwoIns("John", 25);
+        P033_PersonTwoIns alice = new P033_PersonTwoIns("Alice", 30);
         System.out.println("Name: " + john.name + ", Age: " + john.age + ", Name: " + alice.name + ", Age: " +alice.age);
     }
 }
