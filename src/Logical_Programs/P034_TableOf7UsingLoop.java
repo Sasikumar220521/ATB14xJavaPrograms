58ade3e261109ea78c4ce23f88ece022d74b5de9
Sasikumar220521 <sasikumar220521@gmail.com>
2026-06-07 21:38:19 +0530

    UpdatedTableOf7UsingLoop

--- src/Logical_Programs/P034_TableOf7UsingLoop.java
+++ src/Logical_Programs/P034_TableOf7UsingLoop.java
@@ -4,8 +4,8 @@ public class P034_TableOf7UsingLoop {
     public static void main(String[] args) {
         int table = 7;
         for (int i = 1; i <= 10; i++) {
-            System.out.printf("%dx%d=%d", i, table, table * i);
-            System.out.println();
+            System.out.printf("%dx%d=%d", i, table, table * i).println();
+//            System.out.println();
         }
     }
 }
