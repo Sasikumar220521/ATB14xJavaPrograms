c81b3ce7b8b373527554118a749930c8e75a6e9d
Sasikumar220521 <sasikumar220521@gmail.com>
2026-06-07 21:30:51 +0530

    TableOf7UsingLoop

---
+++ src/Logical_Programs/P034_TableOf7UsingLoop.java
@@ -0,0 +1,11 @@
+package Logical_Programs;
+
+public class P034_TableOf7UsingLoop {
+    public static void main(String[] args) {
+        int table = 7;
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%dx%d=%d", i, table, table * i);
+            System.out.println();
+        }
+    }
+}
